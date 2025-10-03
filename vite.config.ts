d090e12340caab8e9c186f772152a1476370cc07
tristin-scalormedical <tristin.shestag@scalormedical.com>
2025-10-03 12:44:50 -0400

    Cleaned up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.cjs',
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  base: './',
   server: {
     allowedHosts: [
       'devserver-main--benevolent-piroshki-5d907a.netlify.app'
