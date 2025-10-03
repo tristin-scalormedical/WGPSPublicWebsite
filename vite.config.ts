57dc8f99351f845c50172f46515e06d3f534d1cd
tristin-scalormedical <tristin.shestag@scalormedical.com>
2025-10-03 12:41:33 -0400

    Updated allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'devserver-main--benevolent-piroshki-5d907a.netlify.app'
+    ]
+  }
 })
